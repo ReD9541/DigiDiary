d6641857680b69210496573cc7a35acc685c06e3
Ritesh Dhungel <139316061+ReD9541@users.noreply.github.com>
2025-04-08 16:29:23 +1000

    Adding more VCs for the app

---
+++ DigiDiary/StreakDetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  StreakDetailVC.swift
+//  DigiDiary
+//
+//  Created by Ritesh Dhungel on 8/4/2025.
+//
+
+import UIKit
+
+class StreakDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
